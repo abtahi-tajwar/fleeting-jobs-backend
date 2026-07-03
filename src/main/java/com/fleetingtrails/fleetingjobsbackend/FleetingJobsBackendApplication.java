dd716560bd29d8367eff8eb53aaba15d18524e4c
MD Abtahi Tajwar <abtahitajwar@mds-macbook-air.home>
2026-07-03 06:00:51 -0400

    Initial upload

---
+++ src/main/java/com/fleetingtrails/fleetingjobsbackend/FleetingJobsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fleetingtrails.fleetingjobsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetingJobsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetingJobsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fleetingtrails/fleetingjobsbackend/FleetingJobsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetingtrails.fleetingjobsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetingJobsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
